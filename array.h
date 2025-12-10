3853c0c028a46f5ef83ad9bc2ffbf6ab3f3634b2
sashafp6 <filippovaalexa@mail.ru>
2025-12-10 20:34:01 +0700

    Update array.h

--- array.h
+++ array.h
@@ -1,1 +1,26 @@
 
+#ifndef ARRAY_H
+#define ARRAY_h
+#include <string>
+
+struct Array{
+    std::string* data; // массив строк
+    int size; // текущий размер массива
+    int capacity; // максимальная вместимость
+ 
+};
+
+Array* createArray(int capacity);
+void pushBack(Array* arr, const std::string& value);
+void insertAt(Array* arr, int index, const std::string& value);
+std::string getAt(Array* arr, int index);
+void removeAt(Array* arr, int index);
+void setAt(Array* arr, int index, const std::string& value);
+void printArray(Array* arr);
+void freeArray(Array* arr);
+
+void loadArrayFromFile(Array* arr, const char* filename);
+void saveArrayToFile(Array* arr, const char* filename);
+void pushBackToFile(Array* arr, const char* filename, const std::string& value);
+
+#endif
